efc14825c5265a9c4c08c008c06c375ddb5dc868
[Carlos Matias] <xentar111@gmail.com>
2024-07-29 19:32:34 -0300

    esta rectangulo

--- ejercicioslogicos/area_de_un_poligono.py
+++ ejercicioslogicos/area_de_un_poligono.py
@@ -6,39 +6,44 @@
 # - Imprime el cálculo del área de un polígono de cada tipo.
 # 
 
-
 #Método Iterativo (con ciclo for):
+import math
 def area_poligono(poligono:tuple):
     if isinstance (poligono, tuple):
         if len(poligono) > 1:
-            lado_base = max(poligono)
+            hipotenusa = max(poligono)
             lado_minimo = min(poligono)
             
             if len(poligono) > 2:
-                #Es un rectangulo
-                pass
-            
-            numeros_borrar = [lado_base, lado_minimo]
-            
-            lado_restante = list(poligono)
-            for lado in numeros_borrar:
-                while lado in lado_restante:
-                    lado_restante.remove(lado)
-            lado_restante = lado_restante[0]
-            #extraer_numeros = {lado_minimo, lado_base}
-            #lado_restante = tuple(x for i, x in enumerate(poligono) if i not in extraer_numeros)
-            #lado_restante = tuple(x for x in poligono not in (lado_base, lado_minimo))
-            
-            print(lado_base)
-            print(lado_minimo)
-            print(lado_restante)
+                #No es un rectangulo
+                numeros_borrar = [hipotenusa, lado_minimo]
                 
+                lado_restante = list(poligono)
+                for lado in numeros_borrar:
+                    while lado in lado_restante:
+                        lado_restante.remove(lado)
+                lado_restante = lado_restante[0]
+                #extraer_numeros = {lado_minimo, hipotenusa}
+                #lado_restante = tuple(x for i, x in enumerate(poligono) if i not in extraer_numeros)
+                #lado_restante = tuple(x for x in poligono not in (hipotenusa, lado_minimo))
+                
+                area = lado_minimo * lado_restante / 2
+                
+                print(hipotenusa)
+                print(lado_minimo)
+                print(lado_restante)#L
+                print(area)
+                #print(math.sqrt(altura))
+            
+            
 
 if __name__ == '__main__':
     #poligono_elegido = input("Ingresa el poligono que deseas: 1.Triangulo, 2.Cuadrado, 3.Rectangulo ")
     #if poligono_elegido == 1:
     #    input("Ahora los tres lados")
     
-    triangulo = (3,2,5)
-    area_poligono(triangulo)
+    triangulo_rectangulo = (10, 6, 8)#este es rectangulo
+    triangulo_equilatero = (2, 5)#este es equilatero
+    cuadrado = (5)#este es equilatero
+    area_poligono(triangulo_rectangulo)
 #Método Recursivo:
